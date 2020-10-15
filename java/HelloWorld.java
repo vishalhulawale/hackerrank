7344a1a8621b1bcdc69de8ed66f8be0881b3f3ff
Vishal Hulawale <hulawale.vishal@Wgmail.com>
2020-10-15 22:19:57 +0530

    Added stdIn and stdOut related solution

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,7 +1,14 @@
+/*
+ Expected o/p:
+ Hello, World.
+ Hello, Java.
+*/
+
 public class HelloWorld {
 	public static void main(String args[]) {
 
-		System.out.println("Hello World");
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
 
 	}
 
